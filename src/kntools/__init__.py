2f52207ce30cc609686116ff86c37d9992bc2b3e
Celia Trunz <30932911+cctrunz@users.noreply.github.com>
2025-02-26 16:59:50 +0100

    update package import

--- src/kntools/__init__.py
+++ src/kntools/__init__.py
@@ -1,6 +1,15 @@
+from kntools.utils import *
+
+#cleanning folder
 from kntools.cleaning.cleaning_fc import *
+
+#import folder
 from kntools.import_fc.import_fc import *
+
+#export folder
 from kntools.export.export_fc import *
 from kntools.export.aven import *
-from kntools.utils import *
+from kntools.export.plot_fc import *
+
+#geometry folder
 from kntools.geometry.geometry_fc import *
